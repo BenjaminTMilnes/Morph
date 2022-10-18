b81bc28b75d4206d62a280949f70364c28b37405
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2022-10-18 18:58:43 +0100

    Added more to MImporter

--- Morph/Morph/MImporter.cs
+++ Morph/Morph/MImporter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using TauParsing;
 
 namespace Morph
@@ -34,6 +35,221 @@ namespace Morph
             var n = (int)character;
 
             return (n >= 48 && n < 58) || (n >= 65 && n < 71) || (n >= 97 && n < 103);
+        }
+
+
+
+        public void ValidateRGBAColourValue(object value)
+        {
+            if (value is MNumber)
+            {
+                var v = int.Parse((value as MNumber).Value);
+
+                if (v < 0 || v > 255)
+                {
+                    throw new MorphSyntaxError("RGBA colour values must be between 0 and 255.");
+                }
+            }
+            if (value is MPercentage)
+            {
+                var v = (value as MPercentage).Value * 100;
+
+                if (v < 0 || v > 100)
+                {
+                    throw new MorphSyntaxError("RGBA colour values must be between 0% and 100%.");
+                }
+            }
+        }
+
+        public string GetHexadecimalNumber(string inputText, Marker marker)
+        {
+            var m = marker.Copy();
+
+            var start = m.P;
+
+            var c = inputText.Substring(m.P, 1)[0];
+
+            if (c != '#')
+            {
+                return null;
+            }
+
+            m.P++;
+
+            while (m.P < inputText.Length)
+            {
+                c = inputText.Substring(m.P, 1)[0];
+
+                if (IsHexadecimal(c))
+                {
+                    m.P++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var end = m.P;
+
+            if (end - start < 2)
+            {
+                return null;
+            }
+
+            var t = inputText.Substring(start, end - start);
+
+            marker.P = m.P;
+
+            return t;
+        }
+
+        public List<object> GetNumberSet(string inputText, Marker marker)
+        {
+            var m = marker.Copy();
+
+            var c = inputText.Substring(m.P, 1);
+
+            if (c != "(")
+            {
+                return null;
+            }
+
+            m.P++;
+
+            var numbers = new List<object>();
+
+            while (true)
+            {
+                GetWhiteSpace(inputText, m);
+
+                var p = GetPercentage(inputText, m);
+
+                if (p != null)
+                {
+                    numbers.Add(p);
+                }
+                else
+                {
+                    var n = GetNumber(inputText, m);
+
+                    if (n != null)
+                    {
+                        numbers.Add(n);
+                    }
+                }
+
+                GetWhiteSpace(inputText, m);
+
+                c = inputText.Substring(m.P, 1);
+
+                if (c == ",")
+                {
+                    m.P++;
+                    continue;
+                }
+                else if (c == ")")
+                {
+                    m.P++;
+                    break;
+                }
+                else
+                {
+                    throw new MorphSyntaxError("Expected a comma or a closing bracket.");
+                }
+            }
+
+            return numbers;
+        }
+
+        public MPercentage GetPercentage(string inputText, Marker marker)
+        {
+            var m = marker.Copy();
+
+            var number = GetNumber(inputText, m);
+
+            if (number == null)
+            {
+                return null;
+            }
+
+            var c = inputText.Substring(m.P, 1);
+
+            if (c != "%")
+            {
+                return null;
+            }
+
+            m.P++;
+            marker.P = m.P;
+
+            return new MPercentage(double.Parse(number.Value) / 100);
+        }
+
+        public MLengthSet GetLengthSet(string inputText, Marker marker)
+        {
+            var m = marker.Copy();
+
+            var lengths = new List<MLength>();
+
+            while (true)
+            {
+                GetWhiteSpace(inputText, m);
+
+                var length = GetLength(inputText, m);
+
+                if (length != null)
+                {
+                    lengths.Add(length);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (!lengths.Any())
+            {
+                return null;
+            }
+
+            marker.P = m.P;
+
+            var lengthSet = new MLengthSet();
+
+            lengthSet.Lengths = lengths;
+
+            return lengthSet;
+        }
+
+        public MLength GetLength(string inputText, Marker marker)
+        {
+            var m = marker.Copy();
+
+            var number = GetNumber(inputText, m);
+
+            if (number == null)
+            {
+                return null;
+            }
+
+            GetWhiteSpace(inputText, m);
+
+            var unit = GetLengthUnit(inputText, m);
+
+            if (unit == null)
+            {
+                return null;
+            }
+
+            marker.P = m.P;
+
+            var length = new MLength();
+
+            length.Number = number;
+            length.Unit = unit;
+
+            return length;
         }
 
         public MNumber GetNumber(string inputText, Marker marker)
