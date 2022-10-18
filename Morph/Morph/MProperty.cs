f5c648624577b0236e9eb51efd81c3b485b9c788
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2022-10-18 02:20:29 +0100

    Project setup

---
+++ Morph/Morph/MProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morph
+{
+    public class MProperty
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public MProperty(string name = "", string value = "")
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Name.Trim(), Value.Trim());
+        }
+    }
+}
